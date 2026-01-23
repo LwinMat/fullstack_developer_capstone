3163df6fadfefa11f7cea66edab9c711f05ea298
LwinMat <lymateo-lopez@myseneca.ca>
2026-01-22 23:43:02 -0500

    Proxy Updates

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -19,7 +19,6 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name
 
-
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
 # Car Models, using ForeignKey field)
