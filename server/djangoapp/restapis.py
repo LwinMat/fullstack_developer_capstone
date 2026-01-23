18bc861660a5095bed39e9b691f17f51603df790
LwinMat <lymateo-lopez@myseneca.ca>
2026-01-23 02:35:30 -0500

    CI/CD

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -12,8 +12,6 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
-# def get_request(endpoint, **kwargs):
-# Add code for get requests to back end
 def get_request(endpoint, **kwargs):
     params = ""
     if(kwargs):
@@ -31,9 +29,7 @@ def get_request(endpoint, **kwargs):
         # If any error occurs
         print("Network exception occurred")
 
-# def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
-# Add code for retrieving sentiments
+
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url + "analyze/" + text
     try:
@@ -44,7 +40,6 @@ def analyze_review_sentiments(text):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
-# def post_review(data_dict):
 # Add code for posting review
 def post_review(data_dict):
     request_url = backend_url + "/insert_review"
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -45,7 +45,7 @@ def logout_request(request):
     logout(request)
 
     # return the empty username
-    data = {"userName":""}
+    data = {"userName": ""}
 
     return JsonResponse(data)
 
@@ -74,7 +74,7 @@ def registration(request):
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
+        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password, email=email)
         # Login the user and redirect to list page
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
@@ -83,11 +83,6 @@ def registration(request):
         data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
-# # Update the `get_dealerships` view to render the index page with
-# a list of dealerships
-# def get_dealerships(request):
-# ...
-#Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
     if(state == "All"):
         endpoint = "/fetchDealers"
@@ -96,12 +91,8 @@ def get_dealerships(request, state="All"):
     dealerships = get_request(endpoint)
     return JsonResponse({ "status": 200, "dealers": dealerships })
 
-# Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
-# ...
 def get_dealer_reviews(request, dealer_id):
-    # if dealer id has been provided
-    # if dealer id has been provided
+    
     if dealer_id:
         endpoint = "/fetchReviews/dealer/" + str(dealer_id)
         reviews = get_request(endpoint)
@@ -116,13 +107,10 @@ def get_dealer_reviews(request, dealer_id):
                 review_detail['sentiment'] = "neutral"
 
         return JsonResponse({"status": 200, "reviews": reviews})
-
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
 
 # Create a `get_dealer_details` view to render the dealer details
-# def get_dealer_details(request, dealer_id):
-# ...
 def get_dealer_details(request, dealer_id):
     if(dealer_id):
         endpoint = "/fetchDealer/" + str(dealer_id)
@@ -132,8 +120,6 @@ def get_dealer_details(request, dealer_id):
         return JsonResponse({"status": 400, "message": "Bad Request"})
 
 # Create a `add_review` view to submit a review
-# def add_review(request):
-# ...
 def add_review(request):
     if(request.user.is_anonymous == False):
         data = json.loads(request.body)
@@ -148,7 +134,7 @@ def add_review(request):
 def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
-    if(count == 0):
+    if (count == 0):
         initiate()
     car_models = CarModel.objects.select_related('car_make')
     cars = []
